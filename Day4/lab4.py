14e64916d67d74618e9127c6c9b0a5d6da67760a
Ken Lowande <kslowande@ksl.dhcp.wustl.edu>
2016-08-15 13:24:58 -0500

    lab4-ksl-working

--- Day4/lab4.py
+++ Day4/lab4.py
@@ -2,9 +2,68 @@
 #Go to the page for each of the professors.
 #Create a .csv file with the following information for each professor:
 # 	-Specialization
-# 	-Name
+# 	-Name ***** got it.
 # 	-Title
 # 	-E-mail
 # 	-Web page
 	
+from bs4 import BeautifulSoup
+import urllib2 
+import random
+import time
+import os
+import re
+
+web_address='https://polisci.wustl.edu/faculty/specialization'
+web_page = urllib2.urlopen(web_address)
+
+soup = BeautifulSoup(web_page.read())
+soup.prettify()
+
+# for loop that prints all names
+names = soup.find_all('a',{'class':"person-view-primary-field" })
+for i in names:
+	print i.get_text()
+
+# function that gets a name	
+def Name(self):
+	return soup.find_all('a',{'class':"person-view-primary-field" })[self].get_text()
+
+
+urls = soup.find_all('a',{'href'})
+for i in names:
+	print i.get_text()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# gets a list of urls
+urls = soup.find_all('href')
+for j in urls:
+	print j.get_text()
+	
+
+# children
+content = soup.find_all('div',{'class':'view-content'})[0]
+content.descendants
+for j in content.children:
+	print j
+
+    
+    
+    
 
