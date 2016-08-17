ae2466b0ec042f14992544e7a58fb7c9d17360c9
Ken Lowande <kslowande@ksl.dhcp.wustl.edu>
2016-08-17 15:26:01 -0500

    ksl-hw2-2-working
    populates title, signatures, policy areas; just need date

--- Homework/HW2/hw2-ksl.py
+++ Homework/HW2/hw2-ksl.py
@@ -9,28 +9,27 @@
 from bs4 import BeautifulSoup
 import urllib2 
 import csv
-
-# open saved web page
-webpg = 'petitions-pg1.htm'
-soup = BeautifulSoup(open(webpg))
+import re
 
 # open active .csv
-with open('wh-petitions.csv', 'wb') as f:
-	my_writer = csv.DictWriter(f, fieldnames=("Name", "Subfield","Title","E-mail","Web-page"))
-	my_writer.writeheader()
+f = open('wh-petitions.csv', 'wb')
+writer = csv.DictWriter(f, fieldnames=("Title", "Signatures","Policy Areas"))
+writer.writeheader()
 
+# saved source code
+pages = ['petitions-pg0.htm','petitions-pg1.htm','petitions-pg2.htm','petitions-pg3.htm']
+ 		
 # master function to get petition info
 def process_pet(div_tag):
 	title=div_tag.find('h3').string
-	signatures=div_tag.find('span',{'class','signatures-number'}).string
+	signatures=div_tag.find('span',{'class':'signatures-number'}).string
 	policy_areas=[]
-	for i in div.tag.find_all('h6'):
+	for i in div_tag.find_all('h6'):
 		policy_areas.append(i.string)
 	policies = ', '.join(policy_areas)
-	url='https://petitions.whitehouse.gov/'+  # div_tag.find('h3')['href']
-#	due_date=profinfo_frompage(prof_address)
-	my_writer.writerow({"Title":title, "Signatures":subfield,"Policy Areas":policy_area,"Due Date":due_date['email']}) 
-
+	writer.writerow({"Title":title.encode("utf-8"), "Signatures":signatures.encode("utf-8"),"Policy Areas":policies.encode("utf-8")}) 
+	
+# "Due Date":due_date['email']
 # function to get due date from petition page
 # def due_frompage(url):
 #	pet_page=urllib2.urlopen(url)
@@ -38,15 +37,13 @@ def process_pet(div_tag):
 #	pet_date = 'NA'
 #	div 
 
-# for loop to loop through each entry
-# for i in soup.find_all('div',{'class':re.compile('views-row+.*') }):
-
+# iterate through all saved webpages and entries in each webpage
+for webpg in pages:
+	soup = BeautifulSoup(open(webpg))
+	entries = soup.find_all('div',{'class':re.compile('views-row+.*') })
+	for entry in entries:
+		process_pet(entry)
+ 		
 f.close()
 
-
-# toy code
-soup.find_all('span',{'class': 'signatures-number' })
-
-title=titles.find('h3').string
-
-url='https://petitions.whitehouse.gov/'+soup.find_all('h3')['href']
+# toy code #
