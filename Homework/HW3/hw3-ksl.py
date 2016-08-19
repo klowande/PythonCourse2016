8869bef70ed05187981aa037e70afd4f0369bc2b
Ken Lowande <kslowande@ksl.dhcp.wustl.edu>
2016-08-19 17:01:30 -0500

    hw3 update

--- Homework/HW3/hw3-ksl.py
+++ Homework/HW3/hw3-ksl.py
@@ -1,13 +1,3 @@
-# For the purposes of this exercise, we define three types of Twitter users. 
-# Layman: Users with less than 100 followers
-# Expert: Users with 100-1000 followers
-# Celebrity: Users with more than 1000 followers
-
-# Two degrees of separation: For the following two questions, limit your search of
-# followers and friends to laymen and experts.
-# Among the followers of your target and their followers, who is the most active? 
-# Among the friends of your target and their friends, who is the most active?
-
 # hw3, 'twit degrees of separation' klowande, 8-19-2016
 
 import tweepy
@@ -15,9 +5,11 @@ import time
 
 # copy/paste or import twitter-auth.py
 
-cml = api.get_user('clairelowande')
+cml = api.get_user('clairelowande') # my wife
 
-# Most active user = total tweets
+# let most active user be the user with the most total tweets
+
+# one degree of separation:
 
 cml_fr = []
 cml_fr_tweet_count = []
@@ -42,24 +34,32 @@ print 'They have %s followers.' % max(cml_fr_follower_count)
 
 cml_followers = dict(zip(cml_fr, cml_fr_follower_count))
 
+# two degrees of separation
+
 twodeg_fr = []
 twodeg_fr_tweet_count = []
 twodeg_fr_follower_count = []
 
 for follower in cml_fr:
-	if cml_followers[follower] > 1000: continue
+	if cml_followers[follower] > 1000: continue # rule to ignore accts w/too many followers
 	user1 = api.get_user(follower)
 	for twodeg_follower in user1.followers_ids():
 		not_finished=True
 		while not_finished:
 			try:
 				user2 = api.get_user(twodeg_follower)
-				if user2.followers_count > 1000: continue
+				if user2.followers_count > 1000: continue # rule to ignore accts w/too many followers
 				twodeg_fr.append(user2.screen_name)
 				twodeg_fr_tweet_count.append(user2.statuses_count)
 				twodeg_fr_follower_count.append(user2.followers_count)
 				not_finished=False
 				print '*'
 			except:
-				print 'Sleeping...'
-				time.sleep(1)
+				print 'Sleeping...' # I like to see that it is doing something.
+				time.sleep(1) 
+				
+print 'The most active friend of friends is %s.' % twodeg_fr[twodeg_fr_tweet_count.index(max(twodeg_fr_tweet_count))]
+print 'They have %s tweets.' % max(twodeg_fr_tweet_count)
+print 'The most popular friend of friends is %s.' % twodeg_fr[twodeg_fr_follower_count.index(max(twodeg_fr_follower_count))]
+print 'They have %s followers.' % max(twodeg_fr_follower_count)
+
