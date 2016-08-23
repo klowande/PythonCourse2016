e11600736df6b625093d70cd00bd430ceac6906b
Ken Lowande <kslowande@ksl.dhcp.wustl.edu>
2016-08-23 16:54:24 -0500

    hw4-finished

--- Homework/HW4/ksl-hw4.py
+++ Homework/HW4/ksl-hw4.py
@@ -2,6 +2,8 @@
 import random
 import itertools
 import time
+import numpy
+import matplotlib.pyplot as plt
 
 # Sorting Algorithm 1: Bogosort - worst of all, according to wikipedia. O((n+1)!)
 
@@ -25,12 +27,13 @@ def order(x):
     for i in range(len(x) - 1):
         if x[i]>x[i+1]: return False
     return True
-    
+
+# Simulation 
 bogo = []
 insert = []
-n = range(1,201)
+n = range(1,11)
  
-for i in range(1,201):
+for i in range(1,11):
 	A = random.sample(range(1,1000),i)
 	B = random.sample(range(1,1000),i)
 	t1 = time.time()
@@ -40,5 +43,19 @@ for i in range(1,201):
 	t3 = time.time()
 	bogo.append(t2-t1)
 	insert.append(t3-t2)
+
+# Plotting
+plt.plot(n,bogo)
+plt.plot(n,insert)
+plt.ylabel('Time (in Seconds)')
+plt.xlabel('List Length (n)')
+plt.legend(['Bogo Sort','Insertion Sort'],loc='upper left')
+plt.show()
+
+# Note: Bogosort is so slow that for n>11, there is no guarantee it will ever sort, so
+# the function goes on forever. It runs out of pseudo-random numbers.
+	
+	
+
 	
 
