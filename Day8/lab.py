c9387b57f3ba0f27853e3ca39cbd546da4f98d87
Ken Lowande <kslowande@ksl.dhcp.wustl.edu>
2016-08-23 11:23:47 -0500

    update-lab

--- Day8/lab.py
+++ Day8/lab.py
@@ -14,15 +14,31 @@ for line in text:
     print line 
 
 # TODO: print all lines that DO NOT contain "the "
+# compile the regular expression
+keyword = re.compile(r"the ")
+
+# search file for keyword, line by line
+for line in text:
+  if not keyword.search(line):
+    print line 
+
 # TODO: print lines that contain a word of any length starting with s and ending with e
-  
-# date = raw_input("Please enter a date in the format MM.DD.YY: ")
-# Print the date input in the following format:
-# Month: MM
-# Day: DD
-# Year: YY
+pattern = re.compile(r'\ss\S*e\s')
+
+# search file for keyword, line by line
+for line in text:
+  if pattern.search(line):
+    print line 
+
+# print dates
+date = raw_input("Please enter a date in the format MM.DD.YY: ")
+dates = re.compile(r'(\d\d).(\d\d).(\d\d)')
+mysearch=dates.search(date)
+calendar=['Month: ','Day: ','Year: ']
+for i in range(1,4): print '%s%s' % (calendar[i-1],mysearch.group(i))
 
 # TODO: Write a regular expression that finds html tags in example.html and print them.
+
 
 # TODO: Scrape a website and search for some things...
 
--- Day8/regular.py
+++ Day8/regular.py
@@ -9,21 +9,23 @@ file.close()
 
 
 
+# rubular.com
+# pythex.com
 
 mytext=''.join(text)
 
-re.split(r'\d',mytext)
-re.split(r'\d\.',mytext)
+re.split(r'\d',mytext) # splits string by numbers and removes the numbers
+re.split(r'\d\.',mytext) # finds a digit followed by a period, and splits like above.
 
 re.findall(r'[a-z]',mytext)
-re.findall(r'[a-z]+',mytext)
+re.findall(r'[a-z]+',mytext) # finds lowercase character, then keeps going until it finds a non-lowercase
 re.findall(r'[A-Z]',mytext)
 re.findall(r'[A-Z]+',mytext)
-re.findall(r'[A-Z]+.',mytext)
+re.findall(r'[A-Z]+.',mytext) # capital letter + any character that comes after it
 re.findall(r'\d',mytext)
 re.findall(r'\d+',mytext)
 re.findall(r'\d+.',mytext)
-re.findall(r'\d+.*',mytext)
+re.findall(r'\d+.*',mytext) # * is as many instances of that as there are. don't use this.
 re.findall(r'\d+.*\w',mytext)
 re.findall(r'\d\w',mytext)
 re.findall(r'(\d+\S*)',mytext)
@@ -62,7 +64,7 @@ re.findall(r'^b\w*',mytext,re.MULTILINE)
 
 mytext = 'a1b2c3D'
 
-re.match(r'\d',mytext) #matches the pattern at the beginning of the string
+re.match(r'\d',mytext) #matches the pattern at the beginning of the string (digit at the beginning, which is not there.)
 re.search(r'\d',mytext) #looks for the pattern anywhere in the string
 
 #math and search
@@ -89,8 +91,8 @@ mysearch.group(0) #the complete match
 mysearch.group(1) #the first group
 mysearch.group(2) #the second group
 
-pattern = re.compile(r'(?P<number>\d*)\s(?P<name>\w*)')
-mysearch=pattern.search(mytext)
+pattern = re.compile(r'(?P<number>\d*)\s(?P<name>\w*)') # creates a dictionary for each group.
+mysearch=pattern.search(mytext) 
 mysearch.groups()
 mysearch.groupdict()
 
